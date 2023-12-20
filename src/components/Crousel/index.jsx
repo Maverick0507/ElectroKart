3213f87995b9003c69b112fa3f6828729c2882ee
Prashant-2505 <shramaprashant2505@gmail.com>
2023-12-20 20:06:37 +0530

    fixed crousel

--- src/components/Crousel/index.jsx
+++ src/components/Crousel/index.jsx
@@ -1,15 +1,12 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import { fetchCategory } from "@/services/admin/category";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y } from 'swiper/modules';
-import 'swiper/swiper-bundle.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+
+import Slider from "react-slick";
 import { useRouter } from "next/navigation";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Index = () => {
   const [category, setCategory] = useState([]);
@@ -72,32 +69,58 @@ const Index = () => {
     }
   }, [category]);
 
-
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
   return (
 
-    <Swiper
-      modules={[Navigation]}
-      spaceBetween={50}
-      slidesPerView={4}
-      navigation
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      className=" h-full  "
-    >
-      {subCategory.map((slide, i) => (
-        <SwiperSlide className=" h-full pl-5 pr-5 overflow-hidden" key={i}
-          onClick={() => route.push(`/products/${slide._id}`)}
-        >
-          <div className=" h-[90%] mx-3 cursor-pointer shadow-md shadow-slate-300 hover:scale-105 duration-400 ease-linear rounded-md">
-            <img
-              className=" h-[80%] w-full rounded-md "
-              src={slide.photos} alt=" product photo" />
-            <p className=" h-[20%] flex justify-center items-center font-semibold uppercase">{slide.name}</p>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div>
+      <div className="w-[100%] p-9">
+        <Slider
+          className=" w-[100%]"
+          {...settings}>
+          {subCategory.map((slide, i) => (
+            <div className=" h-full pl-5 pr-5 overflow-hidden" key={i}
+              onClick={() => route.push(`/products/${slide._id}`)}
+            >
+              <div className=" h-[90%] mx-3 cursor-pointer shadow-md shadow-slate-300 hover:scale-105 duration-400 ease-linear rounded-md">
+                <img
+                  className=" h-[80%] w-full rounded-md "
+                  src={slide.photos} alt=" product photo" />
+                <p className=" h-[20%] flex justify-center items-center font-semibold uppercase">{slide.name}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
   );
 };
 
 export default Index;
+
+{/* <Swiper
+modules={[Navigation]}
+spaceBetween={50}
+slidesPerView={4}
+navigation
+onSwiper={(swiper) => console.log(swiper)}
+onSlideChange={() => console.log('slide change')}
+className=" h-full  "
+>
+{subCategory.map((slide, i) => (
+  <SwiperSlide className=" h-full pl-5 pr-5 overflow-hidden" key={i}
+    onClick={() => route.push(`/products/${slide._id}`)}
+  >
+    <div className=" h-[90%] mx-3 cursor-pointer shadow-md shadow-slate-300 hover:scale-105 duration-400 ease-linear rounded-md">
+      <img
+        className=" h-[80%] w-full rounded-md "
+        src={slide.photos} alt=" product photo" />
+      <p className=" h-[20%] flex justify-center items-center font-semibold uppercase">{slide.name}</p>
+    </div>
+  </SwiperSlide>
+))}
+</Swiper> */}
